942e86a92420898dd1fd3f7cd27cd0f178c49b60
taizentak <bobatacohashi@gmail.com>
2026-09-14 17:31:40 -0700

    Update simple.v

--- src/simple.v
+++ src/simple.v
@@ -22,7 +22,7 @@ module simple(
 	      output reg [6:0] result);
    
 // add your code here
-	always @(*)
+	always @(*) begin
 		case (a)
 			3'b000:result = 7'b0000000;
 			3'b001:result = 7'b0000001;
